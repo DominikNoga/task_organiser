c4377386eb5ecc578d4e73cdb35a8665a4ba773e
Dominik Noga <dominik.noga2001@gmail.com>
2022-10-17 21:52:04 +0200

    changed settings

--- Organiser2/settings.py
+++ Organiser2/settings.py
@@ -123,7 +123,7 @@ STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static')
 ]
 MEDIA_ROOT = os.path.join(BASE_DIR, 'static/img')
-CSRF_TRUSTED_ORIGINS = ['https://web-production-ce34.up.railway.app']
+CSRF_TRUSTED_ORIGINS = ['https://web-production-0c7f.up.railway.app']
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
